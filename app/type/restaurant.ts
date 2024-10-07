e177d186327cb686971c449d91d2fc74d0eec6ac
Passang Dorji <pssngdor94@gmail.com>
2024-10-07 09:23:54 +0600

    adds feat to fetch order by restaurant

---
+++ app/type/restaurant.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+  id: string;
+  name: string;
+  location: string;
+  contact_number: string;
+  account_id: string;
+  inserted_at: string;
+  updated_at: string;
+}
