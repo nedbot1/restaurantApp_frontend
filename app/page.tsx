5d376faa3a49089536153b72885ae47a74cde1e7
tobden <tobdheng@gmail.com>
2024-10-09 16:52:44 +0600

    modifiesui for

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,165 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-export default function LandingPage() {
+// export default function LandingPage() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-[#D9D9D9]/30">
+//       <div className="bg-white rounded-lg border-2 border-black flex flex-col justify-center items-center">
+//       <h1 className="text-4xl font-semibold text-gray-800 mb-6 mt-2">LOGIN</h1>
+//       <div className="flex flex-col gap-4">
+//         <Link
+//           href={"/page/signUp"}
+//           className="bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
+//         >
+//           <h1 className="text-xl">Sign Up If You Are New</h1>
+//         </Link>
+//         <Link
+//           href={"/page/login"}
+//           className="bg-green-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-green-600 transition duration-300"
+//         >
+//           <h1 className="text-xl">Login</h1>
+//         </Link>
+//       </div>
+//       <Link
+//         href={"/app/admin-dashboard"}
+//         className="bg-gray-700 text-white font-semibold py-3 px-6 mt-6 rounded-lg shadow-lg hover:bg-gray-600 transition duration-300"
+//       >
+//         <h1 className="text-xl">Mock Login</h1>
+//       </Link>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+"use client";
+import { UserLogin } from "@/app/services/login";
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import type { LoginResponse } from "@/app/type/login";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userToken, setUserToken] = useState<LoginResponse | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const response = await UserLogin({ email, password });
+      const accountId = response.account.id;
+      localStorage.setItem("accountID", accountId);
+      setUserToken(response);
+      setError(null);
+      router.push("/page/owner");
+    } catch (error) {
+      console.error(error);
+      setError("Failed to login. Please check your credentials.");
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-6">Welcome</h1>
-      <div className="flex flex-col gap-4">
-        <Link
-          href={"/page/signUp"}
-          className="bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
-        >
-          <h1 className="text-xl">Sign Up If You Are New</h1>
-        </Link>
-        <Link
-          href={"/page/login"}
-          className="bg-green-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-green-600 transition duration-300"
-        >
-          <h1 className="text-xl">Login</h1>
-        </Link>
+    <div className="flex bg-[#D9D9D9]/35 rounded-3xl items-center px-14 gap-14 m-14">
+      <div className="absolute top-7 left-14">
+        <img src="/logo.png" alt="Logo" />
       </div>
-      <Link 
-        href={"/app/admin-dashboard"}
-        className="bg-gray-700 text-white font-semibold py-3 px-6 mt-6 rounded-lg shadow-lg hover:bg-gray-600 transition duration-300"
-      >
-        <h1 className="text-xl">Mock Login</h1></Link>
+      <div className="mb-6 ">
+        <img className="h-16 mb-4" src="/logo.png" alt="Logo" />
+        <h1>
+          Create an{" "}
+          <a className="text-[#9C1F1F] font-medium text-2xl">online menu</a> for
+          <br /> your restaurant.
+          <br /> Print a <a className="font-bold text-2xl">QR code</a> that your{" "}
+          <br />
+          customers
+          <br /> can scan to view the menu
+        </h1>
+      </div>
+      <div className="flex h-screen items-center w-2/4 ">
+        <div className="max-w-md w-full p-6 bg-white rounded-2xl border-black border-[3px] shadow-lg flex flex-col justify-center items-center">
+          {/* Login Title */}
+          <h2 className="text-4xl font-bold text-center text-black mb-4 relative border-b-[#F7D109] border-b-2">
+            LOGIN
+            <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-10 h-0.5"></span>
+          </h2>
+          {/* Email Input with Icon */}
+          <div className="relative w-full mb-6">
+            <input
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="w-full py-3 text-xl border-b-2 border-black text-gray-800 font-normal"
+              type="email"
+            />
+            <svg
+              className="absolute right-3 top-3 h-6 w-6 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20 8l-8 5-8-5V6l8 5 8-5M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2z" />
+            </svg>
+          </div>
+
+          {/* Password Input with Icon */}
+          <div className="relative w-full mb-6">
+            <svg
+              className="absolute right-3 top-3 h-6 w-6 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M6 22q-.825 0-1.412-.587T4 20V10q0-.825.588-1.412T6 8h1V6q0-2.075 1.463-3.537T12 1t3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.587 1.413T18 22zm6-5q.825 0 1.413-.587T14 15t-.587-1.412T12 13t-1.412.588T10 15t.588 1.413T12 17M9 8h6V6q0-1.25-.875-2.125T12 3t-2.125.875T9 6z" />
+            </svg>
+            <input
+              placeholder="Password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="w-full py-3 text-xl border-b-2 border-black text-gray-800 font-normal"
+            />
+          </div>
+          {/* Forgot Password Link */}
+          <div className="w-full text-left mb-6">
+            <a href="#" className="text-sm text-gray-600 hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+
+          {/* Login Button */}
+          <button
+            onClick={handleLogin}
+            className="w-full bg-[#006C67] hover:bg-[#005e5a] text-white text-xl font-medium py-3 rounded-lg shadow-md transition duration-200 ease-in-out transform hover:scale-105 mb-6"
+          >
+            Log In
+          </button>
+
+          {/* Error Message */}
+          {error && <div className="mb-4 text-red-500 text-sm">{error}</div>}
+
+          {/* Success Message */}
+          {userToken && (
+            <div className="mt-4 text-center">
+              <p className="text-lg font-medium text-green-600">
+                Login successful!
+              </p>
+              <p>Welcome, {userToken.account.owner_name}</p>
+              <p>Your token: {userToken.token}</p>
+            </div>
+          )}
+
+          {/* Create an Account Link */}
+          <div className="mt-6">
+            <Link
+              href="/page/home"
+              className="text-lg text-black font-light border-b-2 border-black"
+            >
+              Create an Account
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- app/page/owner/page.tsx
+++ app/page/owner/page.tsx
@@ -1,45 +1,53 @@
-"use client"
-import { useEffect, useState } from "react"
-import { fetchOrder, fetchRestaurant, createRestaurant } from "@/app/services/owner"
-import { Order, Restaurant } from "@/app/type/type"
+"use client";
+import { useEffect, useState } from "react";
+import {
+  fetchOrder,
+  fetchRestaurant,
+  createRestaurant,
+} from "@/app/services/owner";
+import { Order, Restaurant } from "@/app/type/type";
+import Navbar from "@/app/component/nav";
+import Link from "next/link";
 
 const OwnerPage: React.FC = () => {
-  const [orders, setOrders] = useState<Order[]>([])
-  const [restaurant, setRestaurant] = useState<Restaurant | null>(null)
-  const [isFetchingOrders, setIsFetchingOrders] = useState(false)
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [restaurant, setRestaurant] = useState<Restaurant | null>(null);
+  const [isFetchingOrders, setIsFetchingOrders] = useState(false);
   const [isPremium, setIsPremium] = useState(false); // To track if the account is premium
   const [isCreatingRestaurant, setIsCreatingRestaurant] = useState(false);
   const [restaurantName, setRestaurantName] = useState("");
   const [restaurantLocation, setRestaurantLocation] = useState("");
   const [restaurantContactNumber, setRestaurantContactNumber] = useState("");
 
-  useEffect(() => {
-    const accountID = localStorage.getItem('accountID');
+  // Load restaurant data on component mount
+  const loadRestaurant = async () => {
+    const accountID = localStorage.getItem("accountID");
     if (accountID) {
-      const loadRestaurant = async () => {
-        try {
-          const response = await fetchRestaurant(accountID);
-          if (response) {
-            setRestaurant(response);
-          } else {
-            setRestaurant(null); // No restaurant exists
-          }
-          // Set isPremium to true or false based on account info (e.g., fetched from response)
-          // setIsPremium(response.isPremium); // Uncomment and modify this based on API response
-        } catch (error) {
-          console.log("Failed to fetch restaurant data", error);
+      try {
+        const response = await fetchRestaurant(accountID);
+        if (response) {
+          setRestaurant(response);
+        } else {
+          setRestaurant(null); // No restaurant exists
         }
-      };
-      loadRestaurant();
+      } catch (error) {
+        console.log("Failed to fetch restaurant data", error);
+      }
     }
+  };
+
+  // Fetch restaurant data on initial mount
+  useEffect(() => {
+    loadRestaurant();
   }, []);
 
+  // Handle fetching orders
   const handleFetchOrders = async () => {
     if (restaurant) {
       setIsFetchingOrders(true);
       try {
         const { data } = await fetchOrder(restaurant.id);
-        console.log(data, "ordered data")
+        console.log(data, "ordered data");
         setOrders(data);
       } catch (error) {
         console.log("Failed to fetch orders", error);
@@ -49,19 +57,22 @@ const OwnerPage: React.FC = () => {
     }
   };
 
+  // Handle restaurant creation
   const handleCreateRestaurant = async (e: React.FormEvent) => {
-    e.preventDefault(); 
-    const accountID = localStorage.getItem('accountID');
+    e.preventDefault();
+    const accountID = localStorage.getItem("accountID");
     if (accountID) {
       try {
         setIsCreatingRestaurant(true);
         const newRestaurant = await createRestaurant({
           name: restaurantName,
           location: restaurantLocation,
-          contact_number: restaurantContactNumber, 
+          contact_number: restaurantContactNumber,
           account_id: accountID,
         });
-        setRestaurant(newRestaurant); 
+        setRestaurant(newRestaurant);
+        // Call loadRestaurant to refresh the restaurant data after creation
+        await loadRestaurant();
       } catch (error) {
         console.log("Failed to create restaurant", error);
       } finally {
@@ -71,79 +82,100 @@ const OwnerPage: React.FC = () => {
   };
 
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold mb-6 text-center">Restaurant Information</h2>
+    <div className="">
+      <div className=" bg-[#D9D9D9]/35 rounded-3xl flex flex-col justify-center px-14 gap-14 m-14 h-screen">
+        {restaurant ? (
+          <div>
+            <h2 className="text-2xl font-bold mb-6 text-center">
+              Restaurant Information
+            </h2>
+            <Link
+              href={`/page/owner/${restaurant.id}`}
+              className="bg-white shadow-lg rounded-lg p-6 border border-gray-200 flex"
+            >
+              <div className="flex flex-col">
+                <p className="text-xl font-semibold text-gray-700">
+                  Name: {restaurant.name}
+                </p>
+                <p className="text-sm text-gray-500">
+                  Location: {restaurant.location}
+                </p>
+                <p className="text-sm text-gray-500">
+                  Contact Number: {restaurant.contact_number}
+                </p>
+              </div>
 
-      {restaurant ? (
-        <div className="bg-white shadow-lg rounded-lg p-6 border border-gray-200">
-          <p className="text-xl font-semibold text-gray-700">Name: {restaurant.name}</p>
-          <p className="text-sm text-gray-500">Location: {restaurant.location}</p>
-          <p className="text-sm text-gray-500">Contact Number: {restaurant.contact_number}</p>
-
-          {/* Fetch Orders Button */}
-          <button
+              {/* Fetch Orders Button */}
+              {/* <button
             onClick={handleFetchOrders}
             className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             disabled={isFetchingOrders}
           >
             {isFetchingOrders ? "Fetching Orders..." : "Fetch Orders"}
-          </button>
-        </div>
-      ) : (
-        <div>
-          <p>No restaurant found. You can create one:</p>
+          </button> */}
+            </Link>
+          </div>
+        ) : (
+          <div className="flex items-center gap-9">
+            <img
+              className=" h-16 mb-4"
+              src="/logo.png"
+              alt="Logo"
+            />
+            {/* Form to create a restaurant */}
+            <form
+              onSubmit={handleCreateRestaurant}
+              className="mt-4 bg-white flex flex-col justify-center items-center w-fit p-10 rounded-xl border border-gray-300 shadow-md"
+            >
+              {/* Title */}
+              <h2 className="text-2xl font-semibold text-black mb-6">
+                Add new restaurant
+              </h2>
 
-          {/* Form to create a restaurant */}
-          <form onSubmit={handleCreateRestaurant} className="mt-4">
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Restaurant Name</label>
+              {/* Restaurant Name Input */}
               <input
                 type="text"
-                placeholder="Restaurant Name"
+                placeholder="Restaurant name"
                 value={restaurantName}
                 onChange={(e) => setRestaurantName(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border border-gray-300 bg-[#E3F2F2] p-3 rounded-lg w-80 mb-4"
                 required
               />
-            </div>
 
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Location</label>
+              {/* Location Input */}
               <input
                 type="text"
-                placeholder="Restaurant Location"
+                placeholder="Location"
                 value={restaurantLocation}
                 onChange={(e) => setRestaurantLocation(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border border-gray-300 bg-[#E3F2F2] p-3 rounded-lg w-80 mb-4"
                 required
               />
-            </div>
 
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Contact Number</label>
+              {/* Contact Number Input */}
               <input
                 type="text"
-                placeholder="Contact Number"
+                placeholder="Contact number"
                 value={restaurantContactNumber}
                 onChange={(e) => setRestaurantContactNumber(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border border-gray-300 bg-[#E3F2F2] p-3 rounded-lg w-80 mb-6"
                 required
               />
-            </div>
 
-            <button
-              type="submit"
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              disabled={isCreatingRestaurant}
-            >
-              {isCreatingRestaurant ? "Creating Restaurant..." : "Create Restaurant"}
-            </button>
-          </form>
-        </div>
-      )}
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="w-full bg-[#006C67] hover:bg-[#005e5a] text-white text-lg font-semibold py-3 rounded-lg shadow-md transition duration-200 ease-in-out transform hover:scale-105"
+                disabled={isCreatingRestaurant}
+              >
+                {isCreatingRestaurant ? "Creating Restaurant..." : "Add +"}
+              </button>
+            </form>
+          </div>
+        )}
 
-      {/* Display orders if there are any */}
-      {orders.length > 0 && (
+        {/* Display orders if there are any */}
+        {/* {orders.length > 0 && (
         <div className="mt-8">
           <h2 className="text-2xl font-bold mb-6 text-center">Orders</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -153,16 +185,28 @@ const OwnerPage: React.FC = () => {
                 className="bg-white shadow-lg rounded-lg p-4 border border-gray-200 hover:shadow-xl transition-shadow duration-300"
               >
                 <div className="mb-4">
-                  <p className="text-xl font-semibold text-gray-700">Total Amount: ${order.total_amount}</p>
-                  <p className="text-sm text-gray-500">Table Number: {order.table_number}</p>
+                  <p className="text-xl font-semibold text-gray-700">
+                    Total Amount: ${order.total_amount}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    Table Number: {order.table_number}
+                  </p>
                 </div>
                 <div className="bg-gray-100 p-3 rounded-lg">
                   {order.order_lists.map((item, index) => (
                     <div key={index} className="mb-4">
-                      <p className="text-lg font-medium text-gray-800">{item.item_name}</p>
-                      <p className="text-sm text-gray-600 mb-2">{item.description}</p>
-                      <p className="text-lg font-semibold text-green-600">Nu.{item.total_price}</p>
-                      <p className="text-lg font-semibold text-green-600">Quantity: {item.quantity}</p>
+                      <p className="text-lg font-medium text-gray-800">
+                        {item.item_name}
+                      </p>
+                      <p className="text-sm text-gray-600 mb-2">
+                        {item.description}
+                      </p>
+                      <p className="text-lg font-semibold text-green-600">
+                        Nu.{item.total_price}
+                      </p>
+                      <p className="text-lg font-semibold text-green-600">
+                        Quantity: {item.quantity}
+                      </p>
                     </div>
                   ))}
                 </div>
@@ -170,9 +214,10 @@ const OwnerPage: React.FC = () => {
             ))}
           </div>
         </div>
-      )}
+      )} */}
+      </div>
     </div>
   );
-}
+};
 
 export default OwnerPage;
